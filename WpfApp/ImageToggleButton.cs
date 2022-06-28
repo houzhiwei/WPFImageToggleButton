a1837d4fd30c22cc590b5698aa6d4605023c5742
DESKTOP-Q3RAO1H\Zack <zack.hou@twviewsonic.onmicrosoft.com>
2022-06-28 08:41:28 +0800

    Add Image ToggleButton

---
+++ WpfApp/ImageToggleButton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    
+    public class ImageToggleButton : ToggleButton
+    {
+        static ImageToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageToggleButton), new FrameworkPropertyMetadata(typeof(ImageToggleButton)));
+        }
+
+        public ImageSource CheckedImageSource
+        {
+            get => (ImageSource)GetValue(CheckedImageSourceProperty);
+            set => SetValue(CheckedImageSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty CheckedImageSourceProperty =
+            DependencyProperty.Register("CheckedImageSource", typeof(ImageSource), typeof(ImageToggleButton), new PropertyMetadata(null));
+
+        public ImageSource UncheckedImageSource
+        {
+            get => (ImageSource)GetValue(UncheckedImageSourceProperty);
+            set => SetValue(UncheckedImageSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty UncheckedImageSourceProperty =
+            DependencyProperty.Register("UncheckedImageSource", typeof(ImageSource), typeof(ImageToggleButton), new PropertyMetadata(null));
+    }
+}
